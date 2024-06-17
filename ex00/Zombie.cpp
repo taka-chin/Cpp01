871cf7c397d0e026ac54c46187ae349fb0cd2bbe
Your Name <you@example.com>
2024-06-17 19:59:48 +0900

    start

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,13 @@
+#ifndef CPP01_EX00_Zombie_H_
+#define CPP01_EX00_Zombie_H_
+
+#include <string>
+
+class Zombie {
+private:
+  std::string name_;
+
+public:
+  void announce(void);
+};
+#endif
---
+++ ex00/Zombie.hpp
@@ -0,0 +1,2 @@
+#include "Zombie.hpp"
+void Zombie::announce() {}
