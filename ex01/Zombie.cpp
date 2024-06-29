88f627ad88ebedf1ba07007520dc0123f92d4b58
Takayuki Yamauchi <tyamauch@c3r6s1.42tokyo.jp>
2024-06-29 11:34:02 +0900

    constructor 実装

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -1,6 +1,10 @@
 #include "Zombie.hpp"
 #include <iostream>
 
+Zombie::Zombie()
+{
+	std::cout << "consructor called" <<std::endl;
+}
 const std::string &Zombie::getName() { return name_; }
 
 void Zombie::setName(std::string name) { name_ = name; }
